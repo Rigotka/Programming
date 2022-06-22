fd149194bae3886753b62b23a57d579b50b3b892
Rigotka <sergey_sorokin.1580@mail.ru>
2022-06-22 19:12:57 +0700

    lab 8

---
+++ StudentsListApplication/Model/Faculty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsListApplication.Model
+{
+    /// <summary>
+    /// Хранит перечисление факультетов.
+    /// </summary>
+    public enum Faculty
+    {
+        Пусто,
+        ФВС,
+        РКФ,
+        РТФ,
+        ФБ
+    }
+}
--- StudentsListApplication/Program.cs
+++ StudentsListApplication/Program.cs
@@ -17,7 +17,7 @@ namespace StudentsListApplication
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
