a55eb87cf351c8461cb2ea5e28d0e92a4267e7cd
Rigotka <sergey_sorokin.1580@mail.ru>
2022-12-09 12:01:03 +0700

    add PriorityOrder class

---
+++ src/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PriorityOrder : Order
+    {
+        private DateTime _deliveryDate;
+
+        private string _deliveryTime;
+
+        public DateTime DeliveryDate
+        {
+            get
+            {
+                return _deliveryDate;
+            }
+            set
+            {
+                _deliveryDate = value;
+            }
+        }
+
+        public string DeliveryTime
+        {
+            get
+            {
+                return _deliveryTime;
+            }
+            set
+            {
+                _deliveryTime = value;
+            }
+        }
+
+        public PriorityOrder(Address address, List<Item> items, 
+            DateTime deliveryDate, string deliveryTime) : base(address, items)
+        {
+            DeliveryDate = deliveryDate;
+            DeliveryTime = deliveryTime;
+        }
+    }
+}
