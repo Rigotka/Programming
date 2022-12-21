ec9aeb943d67b20159f94b2d5c44fed110ebf76d
Rigotka <sergey_sorokin.1580@mail.ru>
2022-12-21 15:22:15 +0700

    add Discount

---
+++ src/ObjectOrientedPractics/Model/PercentDiscount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PercentDiscount
+    {
+        public Category Category { get; set; }
+
+        public double PurchaseAmount { get; set; }
+
+        public int CurrentPercentDiscount { get; set; } = 1;
+
+        public double Calculate(List<Item> items)
+        {
+            double amount = 0;
+            foreach (Item item in items)
+            {
+                if (item.Category == Category)
+                {
+                    amount += item.Cost;
+                }
+            }
+            
+            return amount * ((double)CurrentPercentDiscount / 100);
+        }
+
+        public double Apply(List<Item> items)
+        {
+            return Calculate(items);
+        }
+
+        public void Update(List<Item> items)
+        {
+            double amount = 0;
+            foreach (Item item in items)
+            {
+                if (item.Category == Category)
+                {
+                    amount += item.Cost;
+                }
+            }
+            PurchaseAmount += amount;
+            int newDiscountPercent = (int)(PurchaseAmount / 1000);
+            if (newDiscountPercent <= 10 && newDiscountPercent != 0)
+            {
+                CurrentPercentDiscount = newDiscountPercent;
+            }
+            else if (newDiscountPercent > 10)
+            {
+                CurrentPercentDiscount = 10;
+            }
+        }
+
+        public string Info
+        {
+            get
+            {
+                return $"Процентная «{Category}» - {CurrentPercentDiscount}%";
+            }
+        }
+
+        public PercentDiscount(Category category)
+        {
+            Category = category;
+        }
+    }
+}
---
+++ src/ObjectOrientedPractics/Model/PointsDiscount.cs
@@ -0,0 +1,71 @@
+﻿using ObjectOrientedPractics.Services;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PointsDiscount
+    {
+        private int _points;
+
+        public int Points
+        {
+            get 
+            { 
+                return _points; 
+            }
+            set
+            {
+                ValueValidator.AssertValueOnPositive(value, nameof(Points));
+                _points = value;
+            }
+        }
+
+        public double Calculate(List<Item> items)
+        {
+            double amount = 0;
+            foreach (Item item in items)
+            {
+                amount += item.Cost;
+            }
+
+            if (_points <= (int)(amount * 0.3))
+            {
+                return _points;
+            }
+            else
+            {
+                return Math.Ceiling(amount * 0.3);
+            }
+        }
+
+        public double Apply(List<Item> items)
+        {
+            double discount = Calculate(items);
+            _points -= (int)discount;
+            return discount;
+        }
+
+        public void Update(List<Item> items)
+        {
+            double amount = 0;
+            foreach (var item in items)
+            {
+                amount += item.Cost;
+            }
+
+            _points += (int)Math.Ceiling(amount * 0.1);
+        }
+
+        public string Info
+        {
+            get
+            {
+                return $"Накопительная - {Points} баллов";
+            }
+        }
+
+        public PointsDiscount()
+        {
+
+        }
+    }
+}
