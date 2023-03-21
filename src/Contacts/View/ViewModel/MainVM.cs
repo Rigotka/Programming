873840daeb11f6655fa0c46eaf4ee4193090fc40
Rigotka <sergey_sorokin.1580@mail.ru>
2023-03-21 23:30:16 +0700

    Add class MainVM

---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private Contact _contact;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _contact.FullName;
+            }
+            set
+            {
+                _contact.FullName = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _contact.PhoneNumber;
+            }
+            set
+            {
+                _contact.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _contact.Email;
+            }
+            set
+            {
+                _contact.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
