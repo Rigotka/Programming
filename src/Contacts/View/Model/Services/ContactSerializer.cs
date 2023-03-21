5f777911a487e4f8c82a085b84a68052afde25a9
Rigotka <sergey_sorokin.1580@mail.ru>
2023-03-22 02:52:22 +0700

    Add class ContactSerializer

---
+++ src/Contacts/View/Model/Services/ContactSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace View.Model.Services
+{
+    public class ContactSerializer
+    {
+        public string FileName { get; set; }
+
+        public ContactSerializer(string fileName)
+        {
+            var myDocumentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var fodler = Path.Combine(myDocumentsFolder, "OPP");
+
+            if (!Directory.Exists(fodler))
+                Directory.CreateDirectory(fodler);
+
+            var file = Path.Combine(myDocumentsFolder, $"OPP\\{fileName}.json");
+            if (!File.Exists(file))
+                File.Create(file).Close();
+
+            FileName = file;
+        }
+
+        public void SaveToFile(Contact contact)
+        {
+            using (StreamWriter writer = new StreamWriter(FileName))
+            {
+                writer.Write(JsonConvert.SerializeObject(contact));
+            }
+        }
+
+        public Contact LoadFromFile()
+        {
+            using (StreamReader reader = new StreamReader(FileName))
+            {
+                return JsonConvert.DeserializeObject<Contact>(reader.ReadToEnd()) ?? new Contact();
+            }
+        }
+    }
+}
