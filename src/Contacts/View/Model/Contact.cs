3011bfd615bb112c7d87238509d05a0964f4f4d3
Rigotka <sergey_sorokin.1580@mail.ru>
2023-03-21 22:10:04 +0700

    Add class Contact

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,89 @@
+﻿namespace View.Model
+{
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// ФИО контакта.
+        /// </summary>
+        private string _fullName;
+
+        /// <summary>
+        /// Номер телефона контакта.
+        /// </summary>
+        private string _phoneNumber;
+
+        /// <summary>
+        /// Электронная почта контакта.
+        /// </summary>
+        private string _email;
+
+        /// <summary>
+        /// Возвращает и задет ФИО контакта.
+        /// </summary>
+        public string FullName
+        {
+            get
+            {
+                return _fullName;
+            }
+            set
+            {
+                _fullName = value;
+            }
+        }
+
+        /// <summary>
+        /// возвращает и задает номер телефона контакта.
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает электронную почту контакта.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        /// <summary>
+        /// Создает экземпляр <see cref="Contact"/>
+        /// </summary>
+        public Contact()
+        {
+
+        }
+
+        /// <summary>
+        /// Создает экземпляр <see cref="Contact"/>
+        /// </summary>
+        /// <param name="fullName">ФИО.</param>
+        /// <param name="phoneNumber">Номер телефона.</param>
+        /// <param name="email">Электронная почта.</param>
+        public Contact(string fullName, string phoneNumber, string email)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
